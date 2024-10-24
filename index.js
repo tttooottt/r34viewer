d8d8693377ae24c117031dce2ce5777671edc6b5
tttooottt <tttooo.ttt@mail.ru>
2024-10-24 21:30:34 +0300

    Make it fetch all the vids

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-async function searchVids(e) {
-    const url = "https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&limit=1000&json=1&tags=video"
+async function searchVids(e, limit, pid) {
+    const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index${limit ? "&limit=" + limit : ""}${pid ? "&pid=" + pid : ""}&json=1&tags=video`;
     const res = await fetch(url + " " + e);
     if (!res.ok) {
         console.log(res.status);
         return;
     }
     return res.json();
+}
+
+async function searchVidsAll(e) {
+    let vidsAll = [];
+    let pagesCount = 0;
+    while (true) {
+        console.log(pagesCount);
+        try {
+            const vids = await searchVids(e, 1000, pagesCount++);
+            vidsAll = vidsAll.concat(vids);
+            if (vids.length !== 1000) {
+                break;
+            }
+        }
+        catch {
+            break;
+        }
+    }
+    return vidsAll;
 }
 
 async function setVid(vid) {
@@ -40,17 +59,42 @@ async function start() {
             }
             if (e.target === searchbar) {
                 try {
-                    vids = await searchVids(e.target.value);
-                    setVid(vids[0]);
-                    searchbar.style.outline = "";
-                    currentIndex = 0;
-                    indexInput.value = "";
-                    indexInput.placeholder = currentIndex + 1;
-                    vidsSize.textContent = "/" + vids.length;
+                    vids = await searchVids(e.target.value, 1000);
                 }
                 catch {
                     searchbar.style.outline = "2px solid red";
+                    return;
                 }
+                setVid(vids[0]);
+                searchbar.style.outline = "";
+                currentIndex = 0;
+                indexInput.value = "";
+                indexInput.placeholder = currentIndex + 1;
+                vidsSize.textContent = "/" + vids.length;
+
+                (async function () {
+                    let pagesCount = 0;
+                    while (true) {
+                        try {
+                            const pageVids = await searchVids(e.target.value, 1000, pagesCount++);
+                            vids = vids.concat(pageVids);
+                            indexInput.placeholder = currentIndex + 1;
+                            vidsSize.textContent = "/" + vids.length;
+
+                            if (pageVids.length !== 1000) {
+                                break;
+                            }
+                        }
+                        catch {
+                            break;
+                        }
+                    }
+                })();
+
+                // searchVidsAll(e.target.value).then(vidsAll => {
+                //     vids = vidsAll;
+                //     vidsSize.textContent = "/" + vids.length;
+                // });
             }
             if (e.target === indexInput) {
                 const vid = vids[e.target.value - 1];
