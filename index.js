2668f96372ca16021df26d0750b17f6530a3e6e9
tttooottt <tttooo.ttt@mail.ru>
2024-10-24 19:47:58 +0300

    Show size

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ async function searchVids(e) {
         console.log(res.status);
         return;
     }
-    console.log(res);
     return res.json();
 }
 
@@ -19,26 +18,50 @@ async function setVid(vid) {
 
 async function start() {
     const searchbar = document.querySelector(".searchbar");
+    const indexInput = document.querySelector(".index-input");
+    const vidsSize = document.querySelector(".vids-size");
     let vids = [];
     let currentIndex = 0;
     ['seeking', 'play', 'pause', 'volumechange', 'fullscreenchange'].forEach((event) => {
-        document.querySelector('video').addEventListener(event, (e) => e.target.focus());
+        document.querySelector('video').addEventListener(event, (e) => {
+            if (document.activeElement.nodeName !== "VIDEO") {
+                return;
+            }
+            e.target.focus();
+        });
     });
     document.addEventListener("keydown", async (e) => {
         if (e.key === "Escape") {
             e.target.blur();
         }
         if (e.target.nodeName === "INPUT") {
-            if (e.code === "Enter") {
+            if (e.code !== "Enter") {
+                return;
+            }
+            if (e.target === searchbar) {
                 try {
                     vids = await searchVids(e.target.value);
                     setVid(vids[0]);
                     searchbar.style.outline = "";
                     currentIndex = 0;
+                    indexInput.value = "";
+                    indexInput.placeholder = currentIndex + 1;
+                    vidsSize.textContent = "/" + vids.length;
                 }
                 catch {
                     searchbar.style.outline = "2px solid red";
                 }
+            }
+            if (e.target === indexInput) {
+                const vid = vids[e.target.value - 1];
+                if (!vid) {
+                    return;
+                }
+                setVid(vid);
+                currentIndex = +e.target.value - 1;
+                indexInput.value = "";
+                indexInput.placeholder = currentIndex + 1;
+                console.log(e.target.value);
             }
             return;
         }
@@ -48,12 +71,18 @@ async function start() {
         }
 
         if (e.code === "Comma") {
-            if (0 < currentIndex && currentIndex < vids.length)
+            if (0 < currentIndex && currentIndex < vids.length) {
                 setVid(vids[--currentIndex]);
+                indexInput.value = "";
+                indexInput.placeholder = currentIndex + 1;
+            }
         }
         if (e.code === "Period") {
-            if (0 <= currentIndex && currentIndex < vids.length - 1)
+            if (0 <= currentIndex && currentIndex < vids.length - 1) {
                 setVid(vids[++currentIndex]);
+                indexInput.value = "";
+                indexInput.placeholder = currentIndex + 1;
+            }
         }
     });
 }
