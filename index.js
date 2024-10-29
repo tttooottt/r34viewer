e75fa95eb759f488c44c0857aa37654b74275b26
tttooottt <tttooo.ttt@mail.ru>
2024-10-29 15:46:55 +0300

    Bug fixes, potential realbooru

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-async function searchVids(e, limit, pid) {
-    const url = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index${limit ? "&limit=" + limit : ""}${pid ? "&pid=" + pid : ""}&json=1&tags=video`;
+async function searchVids(host, e, limit, pid) {
+    const url = `https://${host.name}/index.php?page=dapi&s=post&q=index${limit ? "&limit=" + limit : ""}${pid ? "&pid=" + pid : ""}&json=1&tags=video`;
     const res = await fetch(url + " " + e);
     if (!res.ok) {
         console.log(res.status);
         return;
     }
-    return res.json();
+    return res.json().then(v => v.map(host.mapper));
 }
 
-async function searchVidsAll(e) {
+async function searchVidsAll(host, e) {
     let vidsAll = [];
     let pagesCount = 0;
     while (true) {
         try {
-            const vids = await searchVids(e, 1000, pagesCount++);
+            const vids = await searchVids(host, e, 1000, pagesCount++);
             vidsAll = vidsAll.concat(vids);
             if (vids.length !== 1000) {
                 break;
@@ -28,10 +28,41 @@ async function searchVidsAll(e) {
 
 async function setVid(vid) {
     if (!vid) return;
-    document.querySelector("#source").href = "https://rule34.xxx/index.php?page=post&s=view&id=" + vid.id;
+    document.querySelector("#source").href = vid.source;
     const player = document.querySelector("video");
-    player.src = vid.file_url;
+    player.src = vid.url;
     player.play();
+}
+
+function hostedSearhVids (host) {
+    return (...args) => searchVids(host, ...args);
+}
+
+function* cycleHost() {
+    const hosts = [
+        { name: "api.rule34.xxx", icon: "./assets/r34.png", mapper: (video) => {
+            return {
+                url: video.file_url,
+                source: "https://rule34.xxx/index.php?page=post&s=view&id=" + video.id
+            }
+        }},
+        { name: "realbooru.com", icon: "./assets/realbooru.png", mapper: (video) => {
+            return {
+                url: `https://realbooru.com//images/${video.directory}/${video.image}`,
+                source: "https://realbooru.com/index.php?page=post&s=view&id=" + video.id
+            }
+        }}
+    ]
+    
+    for (h of hosts) {
+        h.search = hostedSearhVids(h);
+    }
+
+    while (true) {
+        for (h of hosts) {
+            yield h;
+        }
+    }
 }
 
 async function start() {
@@ -39,8 +70,21 @@ async function start() {
     const indexInput = document.querySelector(".index-input");
     const vidsSize = document.querySelector(".vids-size");
     const videoPlayer = document.querySelector("video");
+    const hostnameEl = document.querySelector("#hostname");
+
+    const hostGen = cycleHost();
+    const nextHost = () => {
+        const h = hostGen.next().value;
+        document.querySelector("link[rel~='icon']").href = h.icon;
+        document.querySelector("#hostname img").src = h.icon;
+        return h;
+    }
+    let currentHost = nextHost();
+
     let vids = [];
     let currentIndex = 0;
+    let searchId = 0;
+
     ['seeking', 'play', 'pause', 'fullscreenchange'].forEach((event) => {
         document.querySelector('video').addEventListener(event, (e) => {
             if (document.activeElement.nodeName !== "VIDEO") {
@@ -49,6 +93,11 @@ async function start() {
             e.target.focus();
         });
     });
+
+    hostnameEl.addEventListener('click', (e) => {
+        currentHost = nextHost();
+    });
+
     document.addEventListener("keydown", async (e) => {
         if (e.key === "Escape") {
             // e.target.blur();
@@ -59,15 +108,18 @@ async function start() {
                 return;
             }
             if (e.target === searchbar) {
+                searchId++;
                 try {
-                    vids = await searchVids(e.target.value, 1000);
+                    vids = await currentHost.search(e.target.value, 1000);
                 }
                 catch {
-                    searchbar.style.outline = "2px solid red";
+                    searchbar.style.borderBottom = "2px solid red";
+                    searchbar.style.marginBottom = "-2px";
                     return;
                 }
                 setVid(vids[0]);
-                searchbar.style.outline = "";
+                searchbar.style.marginBottom = "";
+                searchbar.style.borderBottom = "";
                 currentIndex = 0;
                 indexInput.value = "";
                 indexInput.placeholder = currentIndex + 1;
@@ -75,28 +127,28 @@ async function start() {
                 videoPlayer.focus();
 
                 (async function () {
+                    const localTags = e.target.value;
+                    const localCurrentHost = {...currentHost};
+                    const localSearchId = searchId;
                     let pagesCount = 0;
                     while (true) {
                         try {
-                            const pageVids = await searchVids(e.target.value, 1000, pagesCount++);
+                            const pageVids = await localCurrentHost.search(localTags, 1000, pagesCount++);
+                            if (searchId !== localSearchId) {
+                                return;
+                            }
+                            if (pageVids.length === 0) {
+                                break;
+                            }
                             vids = vids.concat(pageVids);
                             indexInput.placeholder = currentIndex + 1;
                             vidsSize.textContent = "/" + vids.length;
-
-                            if (pageVids.length !== 1000) {
-                                break;
-                            }
                         }
                         catch {
                             break;
                         }
                     }
                 })();
-
-                // searchVidsAll(e.target.value).then(vidsAll => {
-                //     vids = vidsAll;
-                //     vidsSize.textContent = "/" + vids.length;
-                // });
             }
             if (e.target === indexInput) {
                 const vid = vids[e.target.value - 1];
